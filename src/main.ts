473118656d6633d5cb5dff43b95adce400db825f
sahandilshan <sahandilshan222@gmail.com>
2022-01-12 09:22:57 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
+    const ms: string = core.getInput('milliseconds')
+    if (core.isDebug()) {
+      core.debug("Hello World.")
+      core.debug(`Miliseconds: ${ms}`)
+    }
     console.log("Hello World");
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
